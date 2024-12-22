05ccdaa7569fe21c305dc3fb9ce2eb22b5d6d0dd
Chibundu <ndubest56@gmail.com>
2024-12-22 13:07:14 +0100

    more on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,18 @@
+import './footer.css'
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <section></section>
+      <section></section>
+      <section>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12 text-center mb-0'>
+              <p>@ {new Date().getFullYear()} Powered by More-Links Tech</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+  )
 }
