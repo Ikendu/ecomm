1fc33a09605de86c527957e1e26f21690f8f0b72
Chibundu <ndubest56@gmail.com>
2024-12-22 15:35:15 +0100

    more on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,40 @@
+import { BsSearch } from 'react-icons/bs'
 import './footer.css'
 export default function Footer() {
   return (
     <footer>
+      <section className='py-4  '>
+        <div className='container-xxl'>
+          <div className='row align-items-center'>
+            <div className='col-5'>
+              <div className='d-flex'>
+                <img src='' alt='' />
+                <h2></h2>
+              </div>
+            </div>
+            <div className='col-7'>
+              <p>Sign up for Newsletter</p>
+              <div className='input-group'>
+                <input
+                  type='text'
+                  className='form-control py-1'
+                  placeholder='Your Email Address'
+                  aria-describedby='basic-addons2'
+                />
+                <span className='input-group-text p-2' id='basic-addon2'>
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section></section>
-      <section></section>
-      <section>
+      <section className='footersect py-2'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12 text-center mb-0'>
-              <p>@ {new Date().getFullYear()} Powered by More-Links Tech</p>
+              <p className='p-2 m-0'>@ {new Date().getFullYear()} Powered by More-Links Tech</p>
             </div>
           </div>
         </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,64 +1,64 @@
-import "./header.css";
-import { Link } from "react-router-dom";
-import { BsSearch } from "react-icons/bs";
-import Assets from "../../assets/icons/Assets";
+import './header.css'
+import { Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
+import Assets from '../../assets/icons/Assets'
 
-import compIcon from "../../assets/icons/swap.png";
-import favorIcon from "../../assets/icons/love.svg";
-import userIcon from "../../assets/icons/users.svg";
-import cartIcon from "../../assets/icons/cart.svg";
+import compIcon from '../../assets/icons/swap.png'
+import favorIcon from '../../assets/icons/love.svg'
+import userIcon from '../../assets/icons/users.svg'
+import cartIcon from '../../assets/icons/cart.svg'
 
 const links = [
-  { id: 1, name: "Compare", name1: "Products", icon: compIcon },
-  { id: 2, name: "Favourite", name1: "WishList", icon: favorIcon },
-  { id: 3, name: "Login", name1: "My Account", icon: userIcon },
-];
+  { id: 1, name: 'Compare', name1: 'Products', icon: compIcon },
+  { id: 2, name: 'Favourite', name1: 'WishList', icon: favorIcon },
+  { id: 3, name: 'Login', name1: 'My Account', icon: userIcon },
+]
 
 export default function Header() {
   return (
     <>
-      <header className="header-top-strip py-3">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-6 ">
+      <header className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6 '>
               <p>Free shipping for over $100 purchase</p>
             </div>
-            <div className="col-6">
-              <p className="text-end">
-                Hotline: <a href="+23480616322766">+234 8061 632276</a>
+            <div className='col-6'>
+              <p className='text-end'>
+                Hotline: <a href='+23480616322766'>+234 8061 632276</a>
               </p>
             </div>
           </div>
         </div>
       </header>
-      <section className="header-upper py-3">
-        <div className="container-xxl">
-          <div className="row align-items-center">
-            <div className="col-2">
+      <section className='header-upper py-3'>
+        <div className='container-xxl'>
+          <div className='row align-items-center'>
+            <div className='col-2'>
               <h2>
-                <Link className="text-white">More-Links</Link>
+                <Link className='text-white'>More-Links</Link>
               </h2>
             </div>
-            <div className="col-5">
-              <div class="input-group mb-3">
+            <div className='col-5'>
+              <div className='input-group mb-3'>
                 <input
-                  type="text"
-                  class="form-control py-2"
-                  placeholder="Search products here..."
-                  aria-label="Search products here..."
-                  aria-describedby="basic-addon2"
+                  type='text'
+                  className='form-control py-2'
+                  placeholder='Search products here...'
+                  aria-label='Search products here...'
+                  aria-describedby='basic-addon2'
                 />
-                <span class="input-group-text p-3 fs-6" id="basic-addon2">
+                <span className='input-group-text p-3 fs-6' id='basic-addon2'>
                   <BsSearch />
                 </span>
               </div>
             </div>
-            <div className="col-5">
-              <div className="header-upper-links">
+            <div className='col-5'>
+              <div className='header-upper-links'>
                 {links.map((link, idx) => (
                   <div key={idx}>
-                    <Link className="maplinks">
-                      <img src={link.icon} className="right-links" />
+                    <Link className='maplinks'>
+                      <img src={link.icon} className='right-links' />
                       <p>
                         {link.name} <br /> {link.name1}
                       </p>
@@ -66,11 +66,10 @@ export default function Header() {
                   </div>
                 ))}
                 <div>
-                  <Link className="cartIcon">
-                    <img src={cartIcon} className="right-links cart" />
-                    <div className="counter">
-                      <span className="badge bg-white text-dark">{"0"}</span>{" "}
-                      <p>{"$0.00"}</p>
+                  <Link className='cartIcon'>
+                    <img src={cartIcon} className='right-links cart' />
+                    <div className='counter'>
+                      <span className='badge bg-white text-dark'>{'0'}</span> <p>{'$0.00'}</p>
                     </div>
                   </Link>
                 </div>
@@ -80,5 +79,5 @@ export default function Header() {
         </div>
       </section>
     </>
-  );
+  )
 }
